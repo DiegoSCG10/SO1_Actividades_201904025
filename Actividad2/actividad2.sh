940de237f045f9d7ea5b1b39a922e64a8b5d89d2
Diego Saul Camey Giron <78435575+DiegoSCG10@users.noreply.github.com>
2023-02-11 18:41:52 -0600

    Falta analizador

--- Actividad2/actividad2.sh
+++ Actividad2/actividad2.sh
@@ -1,1 +1,43 @@
 # Diego Saul Camey Giron - 201904025
+
+#Leemos la variable GITHUB_USER
+read -p "Ingrese su usuario: " GITHUB_USER
+
+#Se realiza la consulta
+consulta="https://api.github.com/users/$GITHUB_USER"
+resultado=$(curl $consulta)
+
+#Texto de salida
+salida="Hola $GITHUB_USER. User ID:"
+echo $salida
+
+#Obtener fecha de salida
+DIA=$(date +%d)
+MES=$(date +%m)
+ANIO=$(date +%y)
+fecha=$DIA"-"$MES"-"$ANIO
+
+#Creamos la ruta
+ruta="/tmp/$fecha/"
+
+#Creamos la carpeta (si no existe)
+if [ -d $ruta ];
+then
+    echo "La carpeta ya existe"
+else
+    echo "Creando carpeta"
+    mkdir -p $ruta
+fi
+
+#Creamos el archivo si no existe
+rutaArchivo=$ruta"saludos.log"
+if [ -f $rutaArchivo ];
+then
+    echo "El Archivo ya existe"
+else    
+    echo "Creando archivo..."
+    touch $rutaArchivo
+fi    
+
+#Se escribe en el archivo
+echo $salida >> $rutaArchivo
